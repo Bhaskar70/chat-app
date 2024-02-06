2a5df53132003a92afe03b321afc352df9442d90
Bhaskar <bhaskarpaleti70366@gmail.com>
2024-02-06 20:29:43 +0530

    add friend

--- src/app/components/chat-page/chat-page.component.ts
+++ src/app/components/chat-page/chat-page.component.ts
@@ -4,6 +4,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ChatService } from '../../services/chat-service/chat.service';
 import { AuthService } from '../../services/auth-service/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AddfriendComponent } from '../../models/addfriend/addfriend.component';
 
 @Component({
   selector: 'app-chat-page',
@@ -15,7 +17,7 @@ import { AuthService } from '../../services/auth-service/auth.service';
 })
 export class ChatPageComponent {
   userdata:any
-  constructor(private chatserive: ChatService){}
+  constructor(private chatserive: ChatService,   public dialog: MatDialog,){}
   ngOnInit(){
     console.log(this.userdata)
     this.chatserive.getUser()?.subscribe(res=>{
@@ -23,6 +25,7 @@ export class ChatPageComponent {
     })
   }
   addFriend(){
+    const dialogRef = this.dialog.open(AddfriendComponent);
     // const payload = {
     // userId : this.userdata._id , friendName : 'sandeep', friendUserName :'7123456789'
     // }
---
+++ src/app/models/addfriend/addfriend.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-addfriend',
+  standalone: true,
+  imports: [MatDialogModule,MatCardModule,MatFormFieldModule , ReactiveFormsModule ],
+  templateUrl: './addfriend.component.html',
+  styleUrl: './addfriend.component.scss'
+})
+export class AddfriendComponent {
+  'addFriendForm': FormGroup;
+  constructor(
+    public dialogRef: MatDialogRef<AddfriendComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder,
+  ) {
+    this.addFriendForm = this.fb.group({
+      friendName : ['', Validators.required],
+      username: ['', Validators.required],
+    });
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  AddFriend() {
+    if (this.addFriendForm.valid) {
+      this.dialogRef.close(this.addFriendForm.value);
+    }
+  }
+}
